67a8ce82976eec5f9c1c9b242020751f25b96d1b
ajipujo <ajipujohardiyanto@gmail.com>
2022-05-25 19:15:43 +0700

    db baru

--- db_penjualan_iva.sql
+++ db_penjualan_iva.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Bulan Mei 2022 pada 17.45
+-- Waktu pembuatan: 25 Bulan Mei 2022 pada 14.15
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.2
 
@@ -32,12 +32,21 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
   `slug` varchar(500) NOT NULL,
-  `price` int(11) NOT NULL DEFAULT '0',
+  `harga` int(11) NOT NULL DEFAULT '0',
   `stok` int(11) NOT NULL DEFAULT '0',
+  `gambar` varchar(500) NOT NULL,
+  `deskripsi` text NOT NULL,
   `deleted_at` datetime DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `slug`, `harga`, `stok`, `gambar`, `deskripsi`, `deleted_at`, `created_at`, `updated_at`) VALUES
+(5, 'Deskmate Gaming | 300 x 100 cm', 'deskmate-gaming-300-x-100-cm', 450000, 60, 'produk-puAL5Uw4yF-1653473448.jpeg', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus laboriosam adipisci natus numquam tempora cum, sit quas exercitationem accusantium explicabo odit quod, deleniti consequatur porro aliquam. Commodi officiis rerum adipisci.', NULL, '2022-05-25 12:10:48', '2022-05-25 12:10:48');
 
 -- --------------------------------------------------------
 
@@ -87,7 +96,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `role_id`, `email`, `password`, `is_active`, `created_at`, `updated_at`, `deleted_at`) VALUES
 (3, 'superuser', 1, 'superuser@iva.com', '$2y$10$jTTPiJDgIs1vZ2.vxMYT8.8QqNxpe/yRdAX70vtiw82ySZrhwqUAO', 1, '2022-05-19 16:10:44', '2022-05-19 16:10:44', NULL),
-(5, 'dummycustomer2', 3, 'dummycustomer2@iva.com', '$2y$10$tAamrtznyxoXjYLkP0SPO.t1kp7pi9cJsOI6xfqpACd0vMTEWq7Ui', 1, '2022-05-22 12:10:54', '2022-05-22 12:10:54', NULL);
+(5, 'dummycustomer2', 3, 'dummycustomer2@iva.com', '$2y$10$tAamrtznyxoXjYLkP0SPO.t1kp7pi9cJsOI6xfqpACd0vMTEWq7Ui', 1, '2022-05-22 12:10:54', '2022-05-22 12:10:54', NULL),
+(6, 'Aji Pujo Hardiyanto', 3, 'ajipujo@iva.com', '$2y$10$WV7rbNCSlFZn.VF22KVjpOqLbdWsGhgrNpO.g0cd4azhclv3bktva', 1, '2022-05-23 18:01:57', '2022-05-23 18:01:57', NULL);
 
 --
 -- Indexes for dumped tables
@@ -119,7 +129,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `roles`
@@ -131,7 +141,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
