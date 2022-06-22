f0ba6b483b69fa48d7d43feb7b120f4db4148734
ajipujo <ajipujohardiyanto@gmail.com>
2022-06-22 19:09:12 +0700

    Setup composer

--- application/views/components/head.php
+++ application/views/components/head.php
@@ -9,21 +9,21 @@
 	<title><?= $title ? $title : 'Situs Jual Beli Termurah dan Terpercaya' ?> | Vavapedia</title>
 
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="<?= base_url('vendor/bootstrap-5.0.2/dist/css/bootstrap.min.css') ?>">
-	<link rel="stylesheet" href="<?= base_url('vendor/font-awesome-4.7.0/css/font-awesome.min.css') ?>">
-	<link rel="stylesheet" href="<?= base_url('vendor/datepicker/datepicker.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('library/bootstrap-5.0.2/dist/css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('library/font-awesome-4.7.0/css/font-awesome.min.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('library/datepicker/datepicker.css') ?>">
 	<link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?= base_url('vendor/datatables/datatables.min.css') ?>" />
+	<link rel="stylesheet" type="text/css" href="<?= base_url('library/datatables/datatables.min.css') ?>" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600;900&display=swap" rel="stylesheet">
 
 	<script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
-	<script src="<?= base_url('vendor/bootstrap-5.0.2/dist/js/bootstrap.bundle.min.js') ?>"></script>
-	<script src="<?= base_url('vendor/jquery-validation-1.19.3/dist/jquery.validate.min.js') ?>"></script>
-	<script src="<?= base_url('vendor/datatables/datatables.min.js') ?>"></script>
+	<script src="<?= base_url('library/bootstrap-5.0.2/dist/js/bootstrap.bundle.min.js') ?>"></script>
+	<script src="<?= base_url('library/jquery-validation-1.19.3/dist/jquery.validate.min.js') ?>"></script>
+	<script src="<?= base_url('library/datatables/datatables.min.js') ?>"></script>
 	<script src="https://cdn.ckeditor.com/4.13.0/standard/ckeditor.js"></script>
-	<script src="<?= base_url('vendor/datepicker/datepicker.js') ?>"></script>
+	<script src="<?= base_url('library/datepicker/datepicker.js') ?>"></script>
 	
 	<title>Hello, world!</title>
 </head>
